aeb9923e7fd939c0f3e9df8e3bd5f6def144ee51
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2022-02-10 11:46:49 +0100

    up

--- index.js
+++ index.js
@@ -10,13 +10,8 @@ const URL = require('./config/db');
 const routesPlayers = require('./routes/players');
 app.use(cors());
 const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions)) // Use this after the variable declaration//
+app.use(cors()); // Use this after the variable declaration//
 
 
 
